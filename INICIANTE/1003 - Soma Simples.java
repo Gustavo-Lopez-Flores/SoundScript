5b972b566afa7933e4f1fc03bb9877a51801e3e8
Gustavo Lopez Flores <104476623+Gustavo-Lopez-Flores@users.noreply.github.com>
2023-03-06 21:01:03 -0400

    Add files via upload

---
+++ INICIANTE/1003 - Soma Simples.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner ler = new Scanner(System.in);
+        int a = ler.nextInt();
+        int b = ler.nextInt();
+        System.out.printf("SOMA = %d\n", (a+b));
+    }
+}
