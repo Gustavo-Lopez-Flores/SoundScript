bbcf9f1f2043a6b76cd635dc0b93dfacad10db3d
Gustavo Lopez Flores <104476623+Gustavo-Lopez-Flores@users.noreply.github.com>
2023-03-09 12:35:34 -0400

    Add files via upload

---
+++ INICIANTE/1008 - Salário.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner ler = new Scanner(System.in);
+    int numFuncionario = ler.nextInt();
+    int numHorasTrabalhadas = ler.nextInt();
+      
+    double valorRecebidoPorHora = ler.nextDouble();
+    double salarioFuncionario = numHorasTrabalhadas * valorRecebidoPorHora;
+    System.out.printf("NUMBER = %d\n", numFuncionario);
+    System.out.printf("SALARY = U$ %.2f\n", salarioFuncionario);
+  }
+}
