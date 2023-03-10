17d1cc362eb814941a828c62bb9bd2be1ed183ee
Gustavo Lopez Flores <104476623+Gustavo-Lopez-Flores@users.noreply.github.com>
2023-03-10 12:33:21 -0400

    Add files via upload

---
+++ INICIANTE/1011 - Esfera.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner ler = new Scanner(System.in);
+    double raio, volume, pi = 3.14159;
+    raio = ler.nextDouble();
+    volume = (4.0/3) * pi * Math.pow(raio,3);
+    System.out.printf("VOLUME = %.3f\n", volume);
+  }
+}
