7b29a19ace810802f3ade46efab5a75b6292bf04
Gustavo Lopez Flores <104476623+Gustavo-Lopez-Flores@users.noreply.github.com>
2023-03-10 12:32:48 -0400

    Add files via upload

---
+++ INICIANTE/1009 - Salário com Bônus.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner ler = new Scanner(System.in);
+    String nomeVendedor = ler.nextLine();
+    double salarioFixo = ler.nextDouble();
+    double vendasEfetuadas = ler.nextDouble();
+    double recebimentoTotal = salarioFixo + (vendasEfetuadas * 0.15);
+    System.out.printf("TOTAL = R$ %.2f\n", recebimentoTotal);
+  }
+}
---
+++ INICIANTE/1010 - Cálculo Simples.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner ler = new Scanner(System.in);
+    int[] codigoPeca = new int[2];
+    int[] numeroPeca = new int[2];
+    double[] valorPeca = new double[2];
+    double valorTotal = 0;
+    for(int i = 0; i < 2; i++){
+      codigoPeca[i] = ler.nextInt();
+      numeroPeca[i] = ler.nextInt();
+      valorPeca[i] = ler.nextDouble();
+      valorTotal += (valorPeca[i]*numeroPeca[i]); 
+    }
+    System.out.printf("VALOR A PAGAR: R$ %.2f\n", valorTotal);
+  }
+}
