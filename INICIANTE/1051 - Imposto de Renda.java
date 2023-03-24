ef9d0db7cf177b2bb7a0ddd03141d0033065dbfe
Gustavo Lopez Flores <104476623+Gustavo-Lopez-Flores@users.noreply.github.com>
2023-03-24 18:44:21 -0400

    Add files via upload

---
+++ INICIANTE/1051 - Imposto de Renda.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args) {
+    Scanner ler = new Scanner(System.in);
+    double salario, imposto;
+    salario = ler.nextDouble();
+    if( salario >= 0 && salario <= 2000.00){
+      System.out.println("Isento");
+    }else if (salario >= 2000.01 && salario <= 3000.00)
+    {
+      imposto = (salario-2000.00) * 0.08;
+      System.out.printf("R$ %.2f%n",imposto);
+    }else if (salario >= 3000.01 && salario <= 4500.00)
+    {
+      imposto = ((1000.00) * 0.08) + ((salario-3000.00)*0.18);
+      System.out.printf("R$ %.2f%n",imposto);
+    }else if (salario > 4500.00)
+    {
+      imposto = ((1000.00) * 0.08) + (1500*0.18) + ((salario-4500.00)*0.28);
+      System.out.printf("R$ %.2f%n",imposto);
+    }
+    ler.close();
+  }
+}
