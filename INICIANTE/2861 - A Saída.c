775cbebcdd5c8a77efb64073ecc6fdc04646a249
Gustavo Lopez Flores <104476623+Gustavo-Lopez-Flores@users.noreply.github.com>
2023-05-06 16:50:31 -0400

    Add files via upload

---
+++ INICIANTE/2861 - A Saída.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TAM 50
+int main() {
+  int i, n;
+  char p[TAM];
+  while(scanf(" %d",&n) != EOF){
+    scanf(" %[^\n]",p);
+    printf("gzuz\n");
+  }
+  return 0;
+}
