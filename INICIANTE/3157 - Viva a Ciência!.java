8f2fad62f5884804ac4ebce912b06393ca7c2266
Gustavo Lopez Flores <104476623+Gustavo-Lopez-Flores@users.noreply.github.com>
2023-05-22 11:53:25 -0400

    Add files via upload

---
+++ INICIANTE/3157 - Viva a Ciência!.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner ler = new Scanner(System.in);
+    String s;
+    s = ler.nextLine();
+    System.out.println("Nao se refuta ciencia com opiniao");
+    ler.close();
+  }
+}
