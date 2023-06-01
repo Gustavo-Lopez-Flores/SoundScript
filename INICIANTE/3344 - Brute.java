fef31906e8dcf3f1ff7f97d236112922d4ec35bc
Gustavo Lopez Flores <104476623+Gustavo-Lopez-Flores@users.noreply.github.com>
2023-06-01 12:44:35 -0400

    Add files via upload

---
+++ INICIANTE/3344 - Brute.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner ler = new Scanner(System.in);
+    int n;
+    n = ler.nextInt();
+    System.out.println("4");
+    ler.close();
+  }
+}
