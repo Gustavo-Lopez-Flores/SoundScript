67b3eb5f835b75679e144e9f0a10619a8046fbd5
Gustavo Lopez Flores <202319070920@lab01a-l>
2024-04-11 15:21:38 -0400

    Menu e Grid com Adaptador

--- app/src/main/java/com/example/soundscript/MainActivity.java
+++ app/src/main/java/com/example/soundscript/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.view.WindowCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -31,20 +32,9 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
-
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -62,7 +52,11 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if(id==R.id.op_escuro){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            return true;
+        }else if(id==R.id.op_claro) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
             return true;
         }
 
--- app/src/main/java/com/example/soundscript/SecondFragment.java
+++ app/src/main/java/com/example/soundscript/SecondFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -34,6 +35,41 @@ public class SecondFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(SecondFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+        int[] list=new int[] {
+                R.drawable.albuns_arlindo_cruz, R.drawable.albuns_caetano_veloso,R.drawable.albuns_cartola,
+                R.drawable.albuns_chico_buarque, R.drawable.albuns_ed_motta, R.drawable.albuns_frank_sinatra,
+                R.drawable.albuns_grover_washington};
+        binding.grid1.setAdapter(new Adaptador(getContext(),list));
+        binding.grid1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                String album="";
+                switch (position){
+                    case 0:
+                        album="Álbum: Arlindo Cruz";
+                        break;
+                    case 1:
+                        album="Álbum: Caetano Veloso";
+                        break;
+                    case 2:
+                        album="Álbum: Cartola";
+                        break;
+                    case 3:
+                        album="Álbum: Ed Motta";
+                        break;
+                    case 4:
+                        album="Álbum: Frank Sinatra";
+                        break;
+                    case 5:
+                        album="Álbum: Grover Washington";
+                        break;
+                    default:
+                        album="Outros";
+                }
+                binding.textviewSecond.setText(album);
             }
         });
     }
